a0a6f769112b0b89986050908c8348d3e1070a10
Luis-Kr <luis1.kremer@gmail.com>
2025-08-08 00:52:00 +0200

    Adjust formatting

--- anglecam/models/base.py
+++ anglecam/models/base.py
@@ -4,43 +4,50 @@ import torch.nn.functional as F
 from omegaconf import DictConfig
 from typing import List
 
+
 class DINOv2_AngleCam(nn.Module):
     def __init__(self, cfg: DictConfig):
         super().__init__()
         self.cfg = cfg
 
         # Load the backbone
-        self.backbone = torch.hub.load('facebookresearch/dinov2', self.cfg.model.name, pretrained=True)
-        
+        self.backbone = torch.hub.load(
+            "facebookresearch/dinov2", self.cfg.model.name, pretrained=True
+        )
+
         # Freeze the backbone
         for param in self.backbone.parameters():
             param.requires_grad = False
-        
+
         # Unfreeze the transformer blocks specified in the config
         for name, param in self.backbone.named_parameters():
             for block_idx in self.cfg.model.trainable_transformer_blocks:
-                if f'blocks.{block_idx}' in name:
+                if f"blocks.{block_idx}" in name:
                     param.requires_grad = True
                     break
-        
+
         # Create the head
         self.head = self._create_head()
-        
+
     def _create_head(self) -> nn.Module:
         return nn.Sequential(
-            nn.Linear(self.cfg.model.backbone.hidden_dim, self.cfg.model.head.hidden_dims[0]),
+            nn.Linear(
+                self.cfg.model.backbone.hidden_dim, self.cfg.model.head.hidden_dims[0]
+            ),
             nn.Dropout(self.cfg.model.head.dropout),
             nn.GELU(),
-            nn.Linear(self.cfg.model.head.hidden_dims[0], self.cfg.model.output.num_bins),
-            nn.Softmax(dim=1) # Output is a probability distribution over the bins
+            nn.Linear(
+                self.cfg.model.head.hidden_dims[0], self.cfg.model.output.num_bins
+            ),
+            nn.Softmax(dim=1),  # Output is a probability distribution over the bins
         )
-        
+
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         # Forward pass through the backbone
         x = self.backbone(x)
         # Forward pass through the head
         x = self.head(x)
         return x
-    
+
     def _get_trainable_parameters(self) -> List[torch.Tensor]:
         return [p for p in self.parameters() if p.requires_grad]
