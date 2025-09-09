13c6652709f42206ba3aace256471b93828a6866
Luis-Kr <luis1.kremer@gmail.com>
2025-09-09 22:34:21 +0200

    Add download-data script

--- anglecam/cli/train.py
+++ anglecam/cli/train.py
@@ -16,7 +16,6 @@ from anglecam.main import AngleCam
 
 log = logging.getLogger(__name__)
 
-
 @hydra.main(version_base=None, config_path="../config", config_name="main")
 def train(config: DictConfig) -> None:
     """
