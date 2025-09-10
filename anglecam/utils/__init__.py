19628935ac39507e338804c0c4b687231ff3576e
Luis-Kr <luis1.kremer@gmail.com>
2025-09-10 23:24:58 +0200

    Add __init__.py to anglecam/utils for path resolution imports

---
+++ anglecam/utils/__init__.py
@@ -0,0 +1,9 @@
+# anglecam/utils/__init__.py
+from .paths import get_project_root, get_anglecam_root, resolve_data_path, resolve_config_path
+
+__all__ = [
+    "get_project_root",
+    "get_anglecam_root", 
+    "resolve_data_path",
+    "resolve_config_path",
+]
